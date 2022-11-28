6d7973243dd55d177a0809d37c8b51b683b9c32c
shiyd <sydbgx@gmail.com>
2022-11-28 22:14:12 +0800

    修改反向代理内容

--- learn_1/src/ch09/encapsulation/encap_test.go
+++ learn_1/src/ch09/encapsulation/encap_test.go
@@ -1,33 +1,53 @@
 package encapsulation
 
 import (
-	"fmt"
-	"testing"
-	"unsafe"
+   "fmt"
+   "testing"
+   "unsafe"
 )
 
 type Employee struct {
-	Id   string
-	Name string
-	Age  int
+   Id   string
+   Name string
+   Age  int
 }
 
 func TestCreateEmployeeObj(t *testing.T) {
-	e := Employee{"0", "kob", 20}
-	t.Log(e)
+   e0 := Employee{"0", "kob", 20}
+   t.Log(e0)
+   fmt.Printf("e0 Address is %x\n", unsafe.Pointer(&e0.Name))
 
-	e1 := Employee{Name: "Mike", Age: 30}
-	t.Log(e1)
-	t.Log(e1.Id)
+   e1 := Employee{Name: "Mike", Age: 30}
+   t.Log(e1)
+   fmt.Printf("e1 Address is %x\n", unsafe.Pointer(&e1.Name))
 
-	e2 := new(Employee) //返回指针
-	e2.Id = "2"
-	e2.Name = "Rose"
-	e2.Age = 20
-	t.Log(e2)
+   e2 := new(Employee) //返回指针
+   e2.Id = "2"
+   e2.Name = "Rose"
+   e2.Age = 20
+   t.Log(e2)
+   fmt.Printf("e2 Address is %x\n", unsafe.Pointer(&e2.Name))
 
-	t.Logf("e is %T", e)
-	t.Logf("e2 is %T", e2)
+   e3 := &Employee{"3", "Jack", 20}
+   t.Log(e3)
+   fmt.Printf(" e3 Address is %x\n", unsafe.Pointer(&e3.Name))
+
+   e4 := &Employee{"4", "dy", 18}
+   t.Log(e4)
+   fmt.Printf("e4 Address is %x\n", unsafe.Pointer(&e4.Name))
+
+   e5 := &e2
+   (*e5).Name = "test"
+   fmt.Printf("e5 Address is %x\n", unsafe.Pointer(&(*e5).Name))
+   t.Log(e2)
+
+   e6 := &e0
+   fmt.Printf("e6 address is %x\n", unsafe.Pointer(&e6.Name))
+
+   t.Logf("e0 is %T", e0)
+   t.Logf("e1 is %T", e1)
+   t.Logf("e2 is %T", e2)
+   t.Logf("e3 is %T", e3)
 }
 
 //func (e Employee) String() string {
@@ -36,12 +56,12 @@ func TestCreateEmployeeObj(t *testing.T) {
 //}
 
 func (e *Employee) String() string {
-	fmt.Printf("Address is %x", unsafe.Pointer(&e.Name))
-	return fmt.Sprintf("ID:%s/Name:%s/Age:%d", e.Id, e.Name, e.Age)
+   fmt.Printf("Address is %x", unsafe.Pointer(&e.Name))
+   return fmt.Sprintf("ID:%s/Name:%s/Age:%d", e.Id, e.Name, e.Age)
 }
 
 func TestStructOperations(t *testing.T) {
-	e := Employee{"0", "Jack", 20}
-	fmt.Printf("Address is %x\n", unsafe.Pointer(&e.Name))
-	t.Log(e.String())
+   e := Employee{"0", "Jack", 20}
+   fmt.Printf("Address is %x\n", unsafe.Pointer(&e.Name))
+   t.Log(e.String())
 }
--- proxy/reverse_proxy/reverse_server/main.go
+++ proxy/reverse_proxy/reverse_server/main.go
@@ -1,46 +1,47 @@
+// http反向代理
 package main
 
 import (
-   "bufio"
-   "log"
-   "net/http"
-   "net/url"
+	"bufio"
+	"log"
+	"net/http"
+	"net/url"
 )
 
 var (
-   proxyAddr = "http://127.0.0.1:2003"
-   port      = "2002"
+	proxyAddr = "http://127.0.0.1:2003"
+	port      = "2002"
 )
 
 func main() {
-   http.HandleFunc("/", handler)
-   log.Println("Start serving on port " + port)
-   err := http.ListenAndServe(":"+port, nil)
-   if err != nil {
-      log.Fatal(err)
-   }
+	http.HandleFunc("/", handler)
+	log.Println("Start serving on port " + port)
+	err := http.ListenAndServe(":"+port, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-   // step1: 解析代理地址，并更改请求体的协议和主机
-   proxy, err := url.Parse(proxyAddr)
-   r.URL.Scheme = proxy.Scheme
-   r.URL.Host = proxy.Host
+	// step1: 解析代理地址，并更改请求体的协议和主机
+	proxy, err := url.Parse(proxyAddr)
+	r.URL.Scheme = proxy.Scheme
+	r.URL.Host = proxy.Host
 
-   // step2: 请求下游
-   transport := http.DefaultTransport
-   resp, err := transport.RoundTrip(r)
-   if err != nil {
-      log.Print(err)
-      return
-   }
+	// step2: 请求下游
+	transport := http.DefaultTransport
+	resp, err := transport.RoundTrip(r)
+	if err != nil {
+		log.Print(err)
+		return
+	}
 
-   // step3: 把下游请求内容返回给上游
-   for k, v := range resp.Header {
-      for _, v := range v {
-         w.Header().Add(k, v)
-      }
-   }
-   defer resp.Body.Close()
-   bufio.NewReader(resp.Body).WriteTo(w)
+	// step3: 把下游请求内容返回给上游
+	for k, v := range resp.Header {
+		for _, v := range v {
+			w.Header().Add(k, v)
+		}
+	}
+	defer resp.Body.Close()
+	bufio.NewReader(resp.Body).WriteTo(w)
 }
